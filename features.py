e3fcfded5823fd08727210fc152c8123e561bafb
Gabriel Urbain <gabriel.urbain@gmail.com>
2017-08-08 17:35:15 +0200

    Clean up features selection

--- features.py
+++ features.py
@@ -33,8 +33,8 @@ class ReviewsFeaturesExtractor(BaseEstimator, TransformerMixin):
 		self.max_df = float(max_df)
 		self.min_df = float(min_df)
 		self.ngram = ngram
+
 		self.features_names = np.array([])
-		self.dataset = None
 		self.content_fct = None
 
 	def fit(self, x, y=None):
@@ -43,7 +43,7 @@ class ReviewsFeaturesExtractor(BaseEstimator, TransformerMixin):
 
 	def fit_transform(self, dataset):
 		"""
-		Extract and combine all features
+		Compute a new set of features from all dataset fields
 		"""
 
 		# Extract feature matrices
@@ -53,174 +53,18 @@ class ReviewsFeaturesExtractor(BaseEstimator, TransformerMixin):
 			min_df=self.min_df, max_df=self.max_df, stop_words=stop_words)
 
 		tfidf_features = self.content_fct.fit_transform([r.content for r in dataset])
-		names = np.array(self.content_fct.get_feature_names())
+		self.features_names = np.array(self.content_fct.get_feature_names())
 
 		return tfidf_features
-
-		# print "Fill the features matrices"
-		#
-		# # Training phase
-		# if self.features_names.size == 0:
-		# 	ct_ft, ct_nm = self.content_ft(dataset)
-		# 	features_matrix = ct_ft
-		# 	self.features_names = ct_nm
-		#
-		# # Test and validation phase
-		# else:
-		#
-		# 	t_it = time.time()
-		# 	ct_ft, ct_nm = self.content_ft(dataset)
-		# 	print time.time() - t_it
-		# 	t_it = time.time()
-		# 	#ct_ft = ct_ft.tocoo()
-		# 	cols = []
-		# 	rows = []
-		# 	datas = []
-		# 	#print type(ct_ft), ct_ft.nnz, len(ct_ft.data), ct_ft.shape, len( zip(ct_ft.row, ct_ft.col, ct_ft.data))
-		# 	a = 0
-		# 	features_matrix = csr_matrix((len(dataset), self.features_names.size))
-		#
-		# 	for i, n in enumerate(ct_nm):
-		# 		indexes = np.where(self.features_names == n)[0]
-		# 		if indexes.size != 0:
-		# 			old_names_idx = np.where(ct_nm.indices == indexes[0])
-		# 			print old_names_idx
-		# 	new_names_idx = np.where(mat_csr.indices == b)
-		#
-		# 	features_matrix.indices[old_names_idx] = a
-		# 	print
-		# 	return mat_csr.asformat(mat.format)
-
-			# for i, row in enumerate(dataset):
-			#	print np.where(np.in1d(ct_nm, self.features_names))[0], ct_nm.size, self.features_names.size
-			#	features_matrix[row, np.in1d(self.features_names, ct_nm)] = ct_ft.tolil()[row, np.in1d(ct_nm, self.features_names)]
-			#	#print i#, len(np.in1d(self.features_names, ct_nm)), len(self.features_names)
-			# print time.time() - t_it
-
-			#for i, n in enumerate(ct_nm):
-			#		indexes = np.where(self.features_names == n)[0]
-			#		if indexes.size != 0:
-			#			column_sparse = ct_ft[:, i].tocoo()
-			#			for row, data in zip(column_sparse.row, column_sparse.data):
-			#				cols.append(indexes[0])
-			#				rows.append(row)
-			#				datas.append(data)
-			#
-			#			print time.time() - t_it
-
-
-			#for i, j, v in zip(ct_ft.row, ct_ft.col, ct_ft.data):
-			#		if a%100:
-			#				print a
-			#				rows.append(i)
-			#				cols.append(np.where( self.features_names==ct_nm[j] ))
-			#			a += 1
-			# features_matrix = lil_matrix(( ct_ft.data, (ct_ft.row, cols)), shape=(ct_ft.shape[0], self.features_names.size))
-			# print ct_ft.tocsr().shape, features_matrix.shape, np.in1d(self.features_names, ct_nm).shape, np.in1d(ct_nm, self.features_names).shape
-			#for row in range(features_matrix.shape[0]):
-			# features_matrix[:, np.in1d(self.features_names, ct_nm)] = ct_ft.tolil()[:, np.in1d(ct_nm, self.features_names)]
-
-
-		# return features_matrix
 
 	def transform(self, dataset):
-		print "Process the text"
+		"""
+		Compute a set of features for a training dataset according to what has been trained previously
+		"""
 
 		tfidf_features = self.content_fct.transform([r.content for r in dataset])
-		names = np.array(self.content_fct.get_feature_names())
 
 		return tfidf_features
-
-
-	def sort_coo(matrix, names_origin, names_fin):
-		tuples = izip(m.row, m.col, m.data)
-		return sorted(tuples, key=lambda x: (x[0], x[2]))
-
-	def content_ft(self, dataset):
-		"""
-		Create a feature matrix from the content of a review
-		"""
-
-		#names_array = np.array([])
-		#features_matrix = np.array([])
-
-
-
-
-		# for n in range(1, self.ngram+1):
-		#
-		# 	#print "Compute the " + str(n) + "-gram words"
-		#
-		# 	# Create TFIDF vectorizer for ngram words
-		# 	if n >= 4:
-		# 		tfidf_vec = text.TfidfVectorizer(tokenizer=analyze.get_text_ngram, ngram_range=(n, n))
-		# 	else:
-		# 		stop_words = text.ENGLISH_STOP_WORDS.union(["s", "t", "2", "m", "ve"])
-		# 		tfidf_vec = text.TfidfVectorizer(tokenizer=analyze.get_text_ngram, ngram_range=(n, n), stop_words=stop_words)
-		#
-		# 	# Compute the feature matrix
-		# 	tfidf = tfidf_vec.fit_transform([r.content for r in dataset])
-		# 	names = np.array(tfidf_vec.get_feature_names())
-		#
-		# 	# Sort and partition the tfidf matrix of ngram words
-		# 	tfidf_sum = np.array(tfidf.sum(axis=0).transpose().flatten())
-		# 	split_indexes = tfidf_sum.argsort()[0]
-		# 	low_b = int(self.low_boundary * len(split_indexes))
-		# 	up_b = low_b + int(self.frac * len(split_indexes))
-		# 	tfidf_sorted = tfidf[:, split_indexes[low_b:up_b]]
-		# 	names_sorted = names[split_indexes[low_b:up_b]]
-		#
-		# 	# Fill the feature matrix with the values
-		# 	if features_matrix.size == 0:
-		# 		features_matrix = tfidf_sorted
-		# 		names_array = names_sorted
-		# 	else:
-		# 		features_matrix = hstack((features_matrix, tfidf_sorted))
-		# 		names_array = np.hstack((names_array, names_sorted))
-
-		return tfidf, names
-
-	def summary_ft(self):
-		"""
-		Create a feature matrix from the summary of a review
-		"""
-
-		return
-
-	def product_ft(self):
-		"""
-		Create a feature matrix from the product of a review
-		"""
-
-		return
-
-	def author_ft(self):
-		"""
-		Create a feature matrix from the author of a review
-		"""
-
-		return
-
-	def date_ft(self):
-		"""
-		Create a feature matrix from the date of a review
-		"""
-
-		return
-
-	def helpful_ft(self):
-		"""
-		Create a feature matrix from the helpfulness of a review
-		"""
-
-		return
-
-	def get_feature_names(self):
-		"""
-		Return an array with the features name
-		"""
-
-		return self.features_names
 
 
 def save_ft(ft, model, location=DEFAULT_FT_LOCATION):
